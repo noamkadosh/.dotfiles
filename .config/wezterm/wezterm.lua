e6f8dcd3d853814d905b043f3181bc7ab28a1fa6
noam <noamkadosh91@gmail.com>
2023-04-17 15:10:10 -0700

    wezterm: minimal ui configuration

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font("LeagueMono Nerd Font")
+config.font_size = 18.0
+config.line_height = 1.25
+config.color_scheme = "tokyonight_night"
+
+config.use_fancy_tab_bar = false
+config.tab_bar_at_bottom = true
+config.tab_max_width = 24
+config.colors = {
+	tab_bar = {
+		active_tab = {
+			bg_color = "#7aa2f7",
+			fg_color = "#1a1b26",
+			intensity = "Bold",
+		},
+	},
+}
+
+return config
