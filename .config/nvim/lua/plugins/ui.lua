3a33a29f91743328aa2401f58353aebd420b18af
noam <noamkadosh91@gmail.com>
2023-04-27 21:25:18 -0700

    nvim: add guess-indent plugin

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -224,6 +224,14 @@ return {
     },
 
     {
+        "nmac427/guess-indent.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            require("guess-indent").setup({})
+        end,
+    },
+
+    {
         "lukas-reineke/indent-blankline.nvim",
         event = { "BufReadPre", "BufNewFile" },
         config = function()
